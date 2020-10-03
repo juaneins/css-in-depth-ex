1327b219fc611645f29cc058515e0202103ca0ff
juaneins <ballesterosjg@gmail.com>
2020-10-02 21:58:43 -0500

    Create a mobile responsive menu

---
+++ ch8/js/app.js
@@ -0,0 +1,9 @@
+console.log('script app.js');
+(function() {
+let button = document.getElementById('toggle-menu');
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+    let menu = document.getElementById('main-menu');
+    menu.classList.toggle('is-open');
+});
+})();
