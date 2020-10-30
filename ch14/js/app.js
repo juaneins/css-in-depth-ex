28767c3aeb3b7d98472d5b85fcd847f2f606ae7e
juaneins <ballesterosjg@gmail.com>
2020-10-29 19:37:41 -0500

    Transitiotning to auto height

--- ch14/js/app.js
+++ ch14/js/app.js
@@ -1,8 +1,17 @@
 (function() {
     let toggle = document.getElementsByClassName('dropdown__toggle')[0];
     let dropdown = toggle.parentElement;
+    let drawer = document.getElementsByClassName('dropdown__drawer')[0];
+    let height = drawer.scrollHeight;
+    console.log("height:" + height);
+
     toggle.addEventListener('click', function(e) {
         e.preventDefault();
         dropdown.classList.toggle('is-open');
+        if(dropdown.classList.contains('is-open')) {
+            drawer.style.setProperty('height', height + 'px');
+        } else {
+            drawer.style.setProperty('height', '0');
+        }
     });
 }());
