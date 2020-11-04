31a518f965b23990ddaab1bfc323f763e690485d
juaneins <ballesterosjg@gmail.com>
2020-11-03 19:33:10 -0500

    add animation to interact with the user

--- ch16/js/app.js
+++ ch16/js/app.js
@@ -1,10 +1,23 @@
 let input = document.getElementById("trip");
 let button = document.getElementById("submit-button");
 
+let timeout = null;
+
 button.addEventListener("click", function (event) {
   console.log("click on button");
   event.preventDefault();
   button.classList.add("is-loading");
   button.disabled = true;
   input.disabled = true;
+});
+
+input.addEventListener("keyup", function () {
+  clearTimeout(timeout);
+  timeout = setTimeout(function () {
+    button.classList.add("shake");
+  }, 1000);
+});
+
+button.addEventListener("animationend", function () {
+  button.classList.remove("shake");
 });
